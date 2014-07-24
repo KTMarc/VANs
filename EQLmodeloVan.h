897c946c7891a1c4cd3169e75be1d9a9970e3fe7
KTMarc <marc@AirMarc.local>
2014-07-24 17:11:54 +0200

    Tot verge

---
+++ EQLmodeloVan.h
@@ -0,0 +1,13 @@
+//
+//  EQLmodeloVan.h
+//  VANs
+//
+//  Created by Marc Humet on 24/07/14.
+//  Copyright (c) 2014 EQUUS-LIFE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EQLmodeloVan : NSObject
+
+@end
---
+++ EQLmodeloVan.m
@@ -0,0 +1,13 @@
+//
+//  EQLmodeloVan.m
+//  VANs
+//
+//  Created by Marc Humet on 24/07/14.
+//  Copyright (c) 2014 EQUUS-LIFE. All rights reserved.
+//
+
+#import "EQLmodeloVan.h"
+
+@implementation EQLmodeloVan
+
+@end
