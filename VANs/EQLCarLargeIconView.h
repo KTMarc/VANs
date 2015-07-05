c127d1d8ce109d8f75f63a651e9f206e455bbef1
KTMarc <marc.humet@gmail.com>
2015-07-05 15:31:40 -0700

    He colocat el cotxe al Form1, pero hi haura feina amb el autolayout.

---
+++ VANs/EQLCarLargeIconView.h
@@ -0,0 +1,16 @@
+//
+//  EQLCarLargeIconView.h
+//  VAN Selector
+//
+//  Created by Marc Humet on 5/7/15.
+//  Copyright (c) 2015 EQUUS-LIFE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+IB_DESIGNABLE
+
+
+@interface EQLCarLargeIconView : UIView
+
+@end
